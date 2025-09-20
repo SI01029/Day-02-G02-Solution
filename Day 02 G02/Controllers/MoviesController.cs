5f0b9f519215de7dff7544a28ceedd0dba3e9ad4
SI01029 <SaeedIbrahim01029@gmail.com>
2025-09-20 12:32:51 +0300

    Routing

---
+++ Day 02 G02/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Day_02_G02.Controllers
+    //MVC Controller
+{
+    public class MoviesController : Controller
+    {
+        // Actions : Public non-static [Object] Member Methods
+
+        public string GetMovie(int id)
+        {
+            return $" Get Movie with Id: {id}";
+        }
+        public string AddMovie(int id)
+        {
+            return $" Add Movie with Id: {id}";
+        }
+
+    }
+}
--- Day 02 G02/Program.cs
+++ Day 02 G02/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Routing.Constraints;
+
 namespace Day_02_G02
 {
     public class  Program
@@ -5,11 +7,38 @@ namespace Day_02_G02
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            #region Configure Service
+           // builder.Services.AddControllers(); //Register Built in APIs Service
+            builder.Services.AddControllersWithViews(); //Register Built in MVC Service
+           // builder.Services.AddRazorPages(); //Register Built in Razor Page Service
+           // builder.Services.AddMvc(); //Register All Service
+
+            #endregion
+
             var app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            #region Configure Middlewares
+            app.UseRouting();
+
+            //app.MapGet("/", () => "Hello World!");
+            // app.MapGet("/login", () => "you are sigend in!");
+            // app.MapGet("/login",SignIn ); 
+
+            //MVC
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Movies}/{action=GetMovie}/{id:int=12}"  //id optinal id?
+               // defaults: new { Controllers = "Movies", action = "GetMovie", id = 2 }//old syntax
+               //constraints: new {id=new IntRouteConstraint()} //old syntax
+                );
+
+            #endregion
 
             app.Run();
+        }
+        public static string SignIn()
+        {
+            return $"you are sigend in!";
         }
     }
 }
