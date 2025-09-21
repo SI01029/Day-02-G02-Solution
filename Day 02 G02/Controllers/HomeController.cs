8e5a490f3011291db785908611c71458ff68287f
SI01029 <SaeedIbrahim01029@gmail.com>
2025-09-21 10:14:33 +0300

    View

---
+++ Day 02 G02/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Day_02_G02.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet] //Get: base url/Home/index
+        //view : HTML Page
+        //Razor Page :HTML + C#
+        public IActionResult Index()
+        {
+            return View();
+            // return View("Home");
+            //return View("Movie");
+
+        }
+        [HttpGet] //Get: base url/Home/AboutUs
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        [HttpGet] //Get: base url/Home/ContactUs
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        [HttpGet] //Get: base url/Home/Privacy
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    
+}
+
+}
--- Day 02 G02/Program.cs
+++ Day 02 G02/Program.cs
@@ -12,26 +12,29 @@ namespace Day_02_G02
             builder.Services.AddControllersWithViews(); //Register Built in MVC Service
            // builder.Services.AddRazorPages(); //Register Built in Razor Page Service
            // builder.Services.AddMvc(); //Register All Service
+           
 
             #endregion
 
             var app = builder.Build();
 
-            #region Configure Middlewares
-            app.UseRouting();
+            #region Configure Middlewares //middleware for Static Files
+           // app.UseRouting();
 
             //app.MapGet("/", () => "Hello World!");
             // app.MapGet("/login", () => "you are sigend in!");
             // app.MapGet("/login",SignIn ); 
 
             //MVC
+            app.UseStaticFiles();
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Movies}/{action=GetMovie}/{id:int=12}"  //id optinal id?
+                pattern: "{controller=Home}/{action=index}/{id?}"  //id optinal id?
                // defaults: new { Controllers = "Movies", action = "GetMovie", id = 2 }//old syntax
                //constraints: new {id=new IntRouteConstraint()} //old syntax
-                );
 
+                );
+            
             #endregion
 
             app.Run();
