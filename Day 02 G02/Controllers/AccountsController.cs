697cb7e6439cb2644b49012506e1d9b3937266a8
SI01029 <SaeedIbrahim01029@gmail.com>
2025-09-30 00:40:58 +0300

    Layout and View_Start

---
+++ Day 02 G02/Controllers/AccountsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Day_02_G02.Controllers
+{
+    public class AccountsController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+    }
+}
